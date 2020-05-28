7c28986556c99df621e2072926cec88576d8081f
Petr Krysl <pkrysl@ucsd.edu>
2020-05-28 13:19:32 -0700

    five, six

--- tutorials/tutorial5.jl
+++ tutorials/tutorial5.jl
@@ -7,33 +7,33 @@
 
 # As in the previous tutorials, we import a NASTRAN mesh file.
 # This file stores a tetrahedral mesh of a hollow cylinder.
-using MeshPorter: import_NASTRAN
+using MeshSteward: import_NASTRAN
 connectivities = import_NASTRAN("trunc_cyl_shell_0.nas");
 
 # The mesh is created as before by inserting the incidence relation.
-using MeshKeeper: Mesh, insert!
+using MeshSteward: Mesh, insert!
 mesh = Mesh()
 insert!(mesh, connectivities[1]);
 
 # The base incidence relation of the mesh may be retrieved as:
-using MeshKeeper: baseincrel
+using MeshSteward: baseincrel
 ir = baseincrel(mesh);
 
 # Let us look at a summary  of this incidence relation:
-using MeshKeeper: summary
+using MeshSteward: summary
 println(summary(ir))
 
 # We can see that it relates tetrahedral elements with vertices, and the
 # vertices shape collection has an attribute known under the name "geom".
 # We can retrieve this attribute as
-using MeshCore: attribute
-geom = attribute(ir.right, "geom")
+using MeshSteward: geometry
+geom = geometry(mesh)
 
 # Accessing the attribute "geom" provides access to the locations of the
 # vertices, and hence to the geometry of the mesh as it consists of
 # isoparametric elements. This is how we can calculate the bounding box of the
 # mesh.
-using MeshFinder: initbox, updatebox!
+using MeshSteward: initbox, updatebox!
 box = initbox(geom[1])
 for i in 1:length(geom)
     updatebox!(box, geom[i])
--- tutorials/tutorial6.jl
+++ tutorials/tutorial6.jl
@@ -1,39 +1,62 @@
-# # Collecting boundary entities
+# # Collecting and visualizing boundary vertices
 
 # In this tutorial, we will learn
 # 
-#    -  How to access an attribute of an incidence relation.
-#    -  How to find the bounding box of the mesh.
+#    -  How to import an Abaqus file.
+#    -  How to find the vertices on the boundary of the mesh.
+#    -  How to visualize the vertices on the boundary of the mesh.
 
-# As in the previous tutorials, we import a NASTRAN mesh file.
-# This file stores a tetrahedral mesh of a hollow cylinder.
-using MeshPorter: import_ABAQUS
+# In this tutorial we import an Abaqus mesh. It represents a rectangular block
+# with a circular opening, meshed with quadrilaterals. 
+using MeshSteward: import_ABAQUS
 connectivities = import_ABAQUS("block-w-hole.inp");
 
-using MeshKeeper: Mesh, insert!
+# The imported incidence relations are stored in an array, `connectivities`. In
+# the present case is just one such incidence relation in the file, and we will
+# use it to create the mesh of the block.
+using MeshSteward: Mesh, insert!
 mesh = Mesh()
 insert!(mesh, connectivities[1]);
 
-
-using MeshPorter: vtkwrite
-using MeshKeeper: baseincrel
+# We can visualize the mesh by exporting the base relation of the mesh as a VTK
+# file.
+using MeshSteward: vtkwrite
+using MeshSteward: baseincrel
 vtkwrite("block-w-hole", baseincrel(mesh))
 
-using MeshKeeper: boundary
+# The boundary of the mesh would consist of line segments, and may be extracted
+# as:
+using MeshSteward: boundary
 bir = boundary(mesh);
 vtkwrite("block-w-hole-boundary", bir)
 
-using MeshFinder: connectedv
+# To collect all vertices connected by the boundary segments, we can do:
+using MeshSteward: connectedv
 vl = connectedv(bir);
+
+# The result is a list of integer identifiers of the connected vertices. By
+# comparing the length of the list of the vertices on the boundary with the total
+# number of vertices (the number of shapes on the right of the incidence
+# relation), we can see that less than one fourth of the vertices lies on the
+# boundary. 
 using MeshCore: nshapes
 @show length(vl)
 @show nshapes(baseincrel(mesh).right)
 
-using MeshKeeper: vertices
+# The shape collection `baseincrel(mesh).right` represents the vertices in the
+# mesh. The very same collection may be retrieved from the mesh as: 
+using
+MeshSteward: vertices 
 verts = vertices(mesh)
 
+# The incidence relation `(0, 0)` that represents just the vertices on the
+# boundary can be created as a subset of all the vertices in the mesh.
 using MeshCore: subset
 ssverts = subset(verts, vl)
+
+# The vertices on the boundary may be exported for visualization into a VTK
+# file. The entities represented in the file are simply points. They may be
+# visualized with "Point Gaussian" as little balls.
 using MeshCore: nshapes
 @show nshapes(ssverts.left), nshapes(ssverts.right)
 vtkwrite("block-w-hole-vertices", ssverts)
