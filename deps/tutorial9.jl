163887a782a48477c6a74a08866112a5a8749dd0
Petr Krysl <pkrysl@ucsd.edu>
2021-03-10 15:02:09 -0800

    dependencies

--- deps/tutorial9.jl
+++ deps/tutorial9.jl
@@ -2,8 +2,10 @@
 
 # In this tutorial, we will learn
 # 
+#    -  How to create a mesh of hexahedra.
 #    -  How to create a mesh using an implicit definition of the geometry.
-#    -  
+
+# The geometry is a ramp in the form of 3/4 part of a circle.
 
 # The tutorial will produce files for mesh visualization in the 
 # [Paraview](https://www.paraview.org/) format (VTK). One can display this
@@ -11,15 +13,29 @@
 # executed in `mybinder.org`, the graphics file needs to be downloaded to your
 # desktop, and then visualized locally.
 
-# We load the truncated cylindrical shell mesh from a file:
+# We shall use static arrays for the connectivities and the coordinates.
 using StaticArrays
 
+# We start with a parametric domain: a rectangular block, with dimensions of
+# different physical units. The extent of the domain: the first
+# coordinate is angular. The next two coordinates are in the plane generating
+# section.
 L, W, H = 1.5*pi, 3.0, 1.5
-M, N, K = 13, 4, 2
-linix = LinearIndices((M+1, N+1, K+1))
-sn(m, n, k) = linix[m, n, k]
-# sn(m, n, k) = m + (M+1)*(n-1) + (M+1)*(N+1)*(k-1)
+R = 4.0
 
+# This is the number of element edges along each dimension.
+M, N, K = 13, 4, 2
+
+# Now we define the indexing of the vertices. The linear index converts the
+# triple of indexes along the three dimensions into a single number. the
+# dimensions correspond to the number of nodes along direction.
+linix = LinearIndices((M+1, N+1, K+1))
+# The serial-number function converts the triple of indexes into a single linear
+# index.
+sn(m, n, k) = linix[m, n, k]
+
+# Now we create the connectivities of the individual hexahedral cells. The
+# numbering is given by the topologically regular character of the block domain.
 C = SVector{8, Int}[]
 for k in 1:K, n in 1:N, m in 1:M
     c = (
@@ -28,28 +44,54 @@ for k in 1:K, n in 1:N, m in 1:M
         )
     push!(C, SVector{8, Int}(c))
 end
+
+# The locations of the vertices is also perfectly regular within the parametric
+# domain. The nodes are distributed uniformly along each direction. the location
+# in the physical space is computed from the parametric coordinates: the first
+# coordinate is used as an angle, and `R` is the interior radius of the circular
+# ramp.
 loc = SVector{3, Float64}[]
 for k in 1:K+1, n in 1:N+1, m in 1:M+1
-    push!(loc, SVector{3, Float64}((m-1)/M*L, (n-1)/N*W, (k-1)/K*H))
+    a = (m-1)/M*L
+    x, y, z = (R+(n-1)/N*W) * cos(a), (R+(n-1)/N*W) * sin(a), (k-1)/K*H * (1 + a)
+    push!(loc, SVector{3, Float64}(x, y, z))
 end
 
+# At this point we can create the shape collections.
 using MeshCore: ShapeColl, H8, P1
+# This is the collection of hexahedral shapes.
 elements = ShapeColl(H8, length(C), "elements")
+# And this is the collection of the vertices.
 vertices = ShapeColl(P1, length(loc), "vertices")
+
+# The geometry is attached is an attribute. Here we use vector attribute which
+# actually stores three coordinates per vertex.
 using MeshCore: VecAttrib
 vertices.attributes["geom"] = VecAttrib(loc)
 using MeshCore: IncRel
 connectivities = IncRel(elements, vertices, C)
 
 # We now write out a postprocessing file with the elements. Note that we pass
-# the `connectivity` incidence relation to get the elements stored in the
+# the `connectivities` incidence relation to get the elements stored in the
 # file.
 using MeshSteward: vtkwrite
 vtkwrite("block", connectivities)
 
+# Now we will define another shape collection. This one will refer to geometry
+# defined implicitly: through a function that returns the coordinates  of a
+# vertex based on its serial number. Remember that serial numbers are stored in
+# the connectivities.
+
+vertices2 = ShapeColl(P1, prod((M+1, N+1, K+1)), "vertices2")
+
+# The Cartesian indices will allow us to map the linear index to a Cartesian
+# triple of indices.
 cart = CartesianIndices((M+1, N+1, K+1))
 using MeshCore: FunAttrib
-R = 4.0
+
+# Note that this function defines the same geometry in physical space as the
+# loop above. However, the geometry is not stored, it is returned as a triple of
+# numbers.
 access_location(i) = let 
     ci  = cart[i]
     m, n, k = ci[1], ci[2], ci[3]
@@ -57,42 +99,15 @@ access_location(i) = let
     x, y, z = (R+(n-1)/N*W) * cos(a), (R+(n-1)/N*W) * sin(a), (k-1)/K*H * (1 + a)
     SVector{3, Float64}(x, y, z)
 end
-vertices.attributes["geom"] = FunAttrib(0.0, prod((M+1, N+1, K+1)), access_location);
+
+# The geometry attribute is defined by providing access to the locations of the
+# vertices through the function `access_location`.
+vertices2.attributes["geom"] = FunAttrib(0.0, prod((M+1, N+1, K+1)), access_location);
+connectivities = IncRel(elements, vertices2, C)
 vtkwrite("block2", connectivities)
 
-# Compute the boundary incidence relation: the facets bounding the domain.
-using MeshCore: ir_boundary
-bir = ir_boundary(connectivity)
+# Now both data sets have been written out. Load them into
+# [Paraview](https://www.paraview.org/) and it should be possible to verify that
+# those two meshes sets overlap perfectly.
 
-# Select the boundary faces whose normals point in the direction `[0.0, 0.0,
-# 1.0]`, in the sense that the dot product between the normal in this
-# direction  >= 0.99. 
-using MeshSteward: eselect
-el = eselect(bir; facing = true, direction = x -> [0.0, 0.0, 1.0], dotmin = 0.99)
-
-# We know how many triangles compose this part of the boundary: check the length
-# of the list of the triangles.
-@show length(el) == 44
-
-# Now export the triangles as a VTK file. This can now be displayed side-by-side
-# with the original mesh in `"trunc_cyl_shell_0-search.vtk"`. The subset
-# defined by the list `el` from the boundary incidence relation is extracted
-# and passed to `vtkwrite`.
-using MeshCore: ir_subset
-vtkwrite("trunc_cyl_shell_0-search-z=top", ir_subset(bir, el))
-
-# Similar procedure can be applied to select the triangular faces on the
-# cylindrical surface inside the shell. The direction is now defined by taking
-# the location of the point where the normal is calculated, connecting it with
-# the axis of revolution.
-el = eselect(bir; facing = true, direction = x -> [-x[1], -x[2], 0.0], dotmin = 0.99)
-
-# Again, we know how many triangles compose this part of the boundary: check the
-# length of the list of the triangles.
-@show length(el) == 304
-
-# Now export the triangles as a VTK file. This can now be displayed side-by-side
-# with the original mesh in `"trunc_cyl_shell_0-search.vtk"`.
-vtkwrite("trunc_cyl_shell_0-search-interior", ir_subset(bir, el))
-
-
+true
